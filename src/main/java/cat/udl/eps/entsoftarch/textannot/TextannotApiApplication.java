7a266a27185db2cd2f4f62fc67d79232d97eefef
Roberto García <roberto@rhizomik.net>
2018-02-19 15:00:59 +0100

    Spring Initializer

---
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/TextannotApiApplication.java
@@ -0,0 +1,12 @@
+package cat.udl.eps.entsoftarch.textannot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextannotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextannotApiApplication.class, args);
+	}
+}
