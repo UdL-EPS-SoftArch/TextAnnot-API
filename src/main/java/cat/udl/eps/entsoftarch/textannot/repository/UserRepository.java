c6bac28fbc692138ffd5a97cecc2928930558ad0
Roberto García <roberto@rhizomik.net>
2018-02-21 10:21:34 +0100

    Do not expose REST API for User

--- src/main/java/cat/udl/eps/entsoftarch/textannot/repository/UserRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/repository/UserRepository.java
@@ -2,7 +2,9 @@ package cat.udl.eps.entsoftarch.textannot.repository;
 
 import cat.udl.eps.entsoftarch.textannot.domain.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface UserRepository extends PagingAndSortingRepository<User, String> {
 
   User findByEmail(String email);
