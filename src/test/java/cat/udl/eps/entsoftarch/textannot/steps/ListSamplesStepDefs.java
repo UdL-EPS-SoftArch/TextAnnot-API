d96fbbcf603d934a6ff65253bdf14f2c8de01c07
Josep <josep.alos.pascual@hotmail.com>
2018-02-26 16:58:51 +0100

    Corrected ListSamples steps.

--- src/test/java/cat/udl/eps/entsoftarch/textannot/steps/ListSamplesStepDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/textannot/steps/ListSamplesStepDefs.java
@@ -1,17 +1,12 @@
 package cat.udl.eps.entsoftarch.textannot.steps;
 
-import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import cucumber.api.java.en.And;
+import cat.udl.eps.entsoftarch.textannot.domain.Sample;
+import cat.udl.eps.entsoftarch.textannot.repository.SampleRepository;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
@@ -24,14 +19,14 @@ public class ListSamplesStepDefs {
   private SampleRepository sampleRepos;
 
   @When("^I list samples$")
-  public IListSamples() throws Throwable {
+  public void IListSamples() throws Throwable {
     stepDefs.result = stepDefs.mockMvc.perform(
-        get("/samples", samples).accept(MediaType.APPLICATION_JSON))
+        get("/samples").accept(MediaType.APPLICATION_JSON))
     .andDo(print());
   }
 
   @Given("^I create a sample with text \"([^\"]*)\"$")
-  public ICreateASample(String text) throws Throwable {
+  public void ICreateASample(String text) throws Throwable {
     Sample sample = new Sample(text);
     sampleRepos.save(sample);
   }
