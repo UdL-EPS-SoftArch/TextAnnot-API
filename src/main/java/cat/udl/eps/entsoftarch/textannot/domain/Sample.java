171c41ec9234f4775f42945a132ddcf2929f4139
Marc Sances <marcsances@gmail.com>
2018-02-26 16:17:19 +0100

    created CreateSample.feature for writing tests

--- src/main/java/cat/udl/eps/entsoftarch/textannot/domain/Sample.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/domain/Sample.java
@@ -15,21 +15,46 @@ public class Sample {
     private String text;
     
     public Sample() { }
-
+    
+    /**
+     * Constructor for the class.
+     * @param text the text of the sample
+     */
     public Sample(String text) {
         this.text=text;
     }
 
+    /**
+     * Returns the ID for this entity.
+     * @return the id for this entity
+     */
     public int getId() {
         return this.id;
     }
 
+    /**
+     * Returns the text of this sample.
+     * @return the text of this sample
+     */
     public String getText() {
         return this.text;
     }
 
+    /**
+     * Sets the text of this sample.
+     * @param text the text for this sample.
+     */
     public void setText(String text) {
         this.text=text;
     }
+    
+    /**
+     * Returns the string equivalent of this entity.
+     * @return the string equivalent of this entity.
+     */
+    public String toString() {
+        return this.text;
+    }
+
 
 }
