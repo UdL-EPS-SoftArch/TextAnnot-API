4a8235ff3a96929c7d352f1b4ff431e3acfcc2b1
Rogercapcas <rogercapcas@gmail.com>
2018-02-27 17:45:11 +0100

    First push, initial version

---
+++ src/test/java/cat/udl/eps/entsoftarch/textannot/steps/ListMetadataTemplatesDefs.java
@@ -0,0 +1,12 @@
+package cat.udl.eps.entsoftarch.textannot.steps;
+
+import cucumber.api.PendingException;
+
+public class ListMetadataTemplatesDefs {
+    public ListMetadataTemplatesDefs() {
+        And("^There is a metadata template with name \"([^\"]*)\"$", (String arg0) -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+    }
+}
