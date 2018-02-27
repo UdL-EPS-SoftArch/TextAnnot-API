1f4401aa875134eed70ac8971849ffda45e4f2c4
Sergio Salcedo <trugove@hotmail.com>
2018-02-27 18:21:32 +0100

    Created Steps for CreateMetadataTemplate feature(Not implemented).

--- src/test/java/cat/udl/eps/entsoftarch/textannot/steps/CreateMetadataTemplateDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/textannot/steps/CreateMetadataTemplateDefs.java
@@ -1,7 +1,19 @@
 package cat.udl.eps.entsoftarch.textannot.steps;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
 
 public class CreateMetadataTemplateDefs {
+    @When("^I create a new Metadata Template with name \"([^\"]*)\"$")
+    public void iCreateANewMetadataTemplateWithName(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^The object name is \"([^\"]*)\"$")
+    public void theObjectNameIs(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
 }
