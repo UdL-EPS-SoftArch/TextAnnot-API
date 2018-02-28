f48b960e8ec31c3002963af9c1a64afd2895360d
Rogercapcas <rogercapcas@gmail.com>
2018-02-28 11:14:21 +0100

    First push, initial version

--- src/test/java/cat/udl/eps/entsoftarch/textannot/steps/ListMetadataTemplatesDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/textannot/steps/ListMetadataTemplatesDefs.java
@@ -1,12 +1,43 @@
 package cat.udl.eps.entsoftarch.textannot.steps;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
 
 public class ListMetadataTemplatesDefs {
-    public ListMetadataTemplatesDefs() {
-        And("^There is a metadata template with name \"([^\"]*)\"$", (String arg0) -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
-        });
+    @When("^I retrieve all metadata templates$")
+    public void iRetrieveAllMetadataTemplates() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^There is a metadata template with name \"([^\"]*)\"$")
+    public void thereIsAMetadataTemplateWithName(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^The respone contains a MetadataTemplate with name \"([^\"]*)\"$")
+    public void theResponeContainsAMetadataTemplateWithName(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I retrieve all MetadataTemplate$")
+    public void iRetrieveAllMetadataTemplate() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^The response is a MetadataList\[(\\d+)\]$")
+    public void theResponseIsAMetadataList(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^the response contains a MetadataTemplate with name \"([^\"]*)\"$")
+    public void theResponseContainsAMetadataTemplateWithName(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
     }
 }
