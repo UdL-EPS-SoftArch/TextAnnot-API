b6a7ff45c819b84749b06f01832c3759c69a5449
Rogercapcas <rogercapcas@gmail.com>
2018-02-28 20:18:10 +0100

    First push, initial version

--- src/test/java/cat/udl/eps/entsoftarch/textannot/steps/ListMetadataTemplatesDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/textannot/steps/ListMetadataTemplatesDefs.java
@@ -1,13 +1,33 @@
 package cat.udl.eps.entsoftarch.textannot.steps;
 
+import cat.udl.eps.entsoftarch.textannot.domain.MetadataTemplate;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
 
+//
+//Tal i com ho faria jo:
+//  1.- Saber amb qui interactuem perquè no sabem de qui hem de rebre la resposta, ni com es demana aquesta resposta.
+//      Entenc que un requisit que necessitariem és que existeixi l'element que guarda i retorna les MetadataTemplate's,
+//      per tant un requisit més que s'hauria d'afegir a la feature seria que existís aquest element, també.
+//
+//  2.- En el cas de la funció de la linia 10(la primera de la classe), si l'objecte en qüestió tingui també
+//      un mètode per crear instàncies de MetadataTemplate, s'utilitzarà per crear-ne una i guardar-la.
+//      En cas contrari, amb el mètode pertinent de l'objecte guardarem la instància de MetadataTemplate
+//      creada.
+//
+//  3.- En el cas de la última funció de la classe, si l'objecte en qüestió te un mètode per crear
+//      instàncies de MetadataTemplate, amb un bucle es creen i es guarden tantes instàncies com ens indiqui
+//      l'argument del mètode que arrbi. En cas contrari, Es crea un array de MetadataTemplate amb tantes
+//      posicions com indiqui l'argument, del mètode, que s'ha passat. Per cada posició es crea una
+//      instància, i al sortir del bucle es guarden una a una amb l'objecte en qüestió.
+//
+
+
 public class ListMetadataTemplatesDefs {
     @And("^There is a metadata template with name \"([^\"]*)\"$")
-    public void thereIsAMetadataTemplateWithName(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+    public void thereIsAMetadataTemplateWithName(String metadataTemplateName) throws Throwable {
+        MetadataTemplate mt = new MetadataTemplate(metadataTemplateName);
         throw new PendingException();
     }
 
@@ -45,5 +65,11 @@ public class ListMetadataTemplatesDefs {
     public void theResponseIsAMetadataList() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
+    }
+
+    @And("^There are (\\d+) MetadataTemplates$")
+    public void thereAreMetadataTemplates(int numOfMetadataTemplates) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
     }
 }
--- src/test/resources/features/ListMetadataTemplates.feature
+++ src/test/resources/features/ListMetadataTemplates.feature
@@ -9,17 +9,14 @@ Feature: List Metadata Template
 
   Scenario: List a metadata template as admin
     Given I login as "admin" with password "password"
-    And There is a MetadataTemplate with name "name"
+    And There is a metadata template with name "name"
     When I retrieve all metadata templates
     Then The response code is 200
     And The respone contains a MetadataTemplate with name "name"
 
   Scenario: List 4 metadata templates as admin
     Given I login as "admin" with password "password"
-    And There is a MetadataTemplate with name "name1"
-    And There is a MetadataTemplate with name "name2"
-    And There is a MetadataTemplate with name "name3"
-    And There is a MetadataTemplate with name "name4"
+    And There are 4 MetadataTemplates
     When I retrieve all MetadataTemplate
     Then The response code is 200
     And The response is a MetadataTemplate
@@ -33,10 +30,7 @@ Feature: List Metadata Template
 
   Scenario: List 4 metadata templates as linguist
     Given I login as "linguist" with password "password"
-    And There is a MetadataTemplate with name "name1"
-    And There is a MetadataTemplate with name "name2"
-    And There is a MetadataTemplate with name "name3"
-    And There is a MetadataTemplate with name "name4"
+    And There are 4 MetadataTemplates
     When I retrieve all MetadataTemplate
     Then The response code is 200
     And The response is a MetadataList
