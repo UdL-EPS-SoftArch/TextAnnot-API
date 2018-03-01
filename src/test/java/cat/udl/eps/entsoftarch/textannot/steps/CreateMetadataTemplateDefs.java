cf1dafad0507635da45587db7f01c699de00cfca
Sergio Salcedo <trugove@hotmail.com>
2018-03-01 16:28:11 +0100

    Tests almost done, missing emptyName test.

--- src/test/java/cat/udl/eps/entsoftarch/textannot/steps/CreateMetadataTemplateDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/textannot/steps/CreateMetadataTemplateDefs.java
@@ -1,6 +1,7 @@
 package cat.udl.eps.entsoftarch.textannot.steps;
 
 import cat.udl.eps.entsoftarch.textannot.domain.MetadataTemplate;
+import cat.udl.eps.entsoftarch.textannot.repository.MetadataTemplateRepository;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
@@ -9,18 +10,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 public class CreateMetadataTemplateDefs {
 
     @Autowired
     private StepDefs stepDefs;
+
     protected ResultActions result;
+    private MetadataTemplateRepository repository;
 
     @When("^I create a new Metadata Template with name \"([^\"]*)\"$")
     public void iCreateANewMetadataTemplateWithName(String arg0) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
+
+        //MetadataTemplate mt = new MetadataTemplate("Sonia");
+        //repository.save(mt);
+
         JSONObject metadataTemplate = new JSONObject();
         metadataTemplate.put("name", arg0);
         stepDefs.result = stepDefs.mockMvc.perform(
@@ -33,7 +43,13 @@ public class CreateMetadataTemplateDefs {
     }
 
     @And("^The object name is \"([^\"]*)\"$")
-    public void theObjectNameIs(String arg0) throws Throwable {
+    public void theObjectNameIs(String name) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get("/metadataTemplates/{name}", name)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print())
+                .andExpect(jsonPath("$.name", is(name)));
     }
 }
