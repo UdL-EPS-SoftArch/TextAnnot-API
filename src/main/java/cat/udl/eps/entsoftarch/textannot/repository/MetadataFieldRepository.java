3010c5e6600655fd5b1b963555049f3ceb363642
oriol <oriolqueraltamoros@gmail.com>
2018-03-05 17:00:05 +0100

    Class rename MetadataFieldRepository

--- src/main/java/cat/udl/eps/entsoftarch/textannot/repository/MetadataFieldRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/repository/MetadataFieldRepository.java
@@ -1,10 +1,10 @@
 package cat.udl.eps.entsoftarch.textannot.repository;
 
 
-import cat.udl.eps.entsoftarch.textannot.domain.AddMetaDataField;
+import cat.udl.eps.entsoftarch.textannot.domain.MetadataField;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface AddMetadataFieldRepository extends PagingAndSortingRepository<AddMetaDataField, Integer> {
+public interface MetadataFieldRepository extends PagingAndSortingRepository<MetadataField, Integer> {
 }
