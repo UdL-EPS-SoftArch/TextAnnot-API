b853d3b4da2aec8c719a248a6dfbaa5b83a458b3
crisrivas <cris.rf95@gmail.com>
2018-03-05 16:54:46 +0100

    tag @RepositoryRestResource

--- src/main/java/cat/udl/eps/entsoftarch/textannot/repository/SampleRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/repository/SampleRepository.java
@@ -3,7 +3,9 @@ package cat.udl.eps.entsoftarch.textannot.repository;
 import cat.udl.eps.entsoftarch.textannot.domain.Sample;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface SampleRepository extends PagingAndSortingRepository<Sample, Integer> {
 
     Sample findByTextContains (@Param("word") String word);
