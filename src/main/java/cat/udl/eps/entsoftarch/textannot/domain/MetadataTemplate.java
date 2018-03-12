107ad3ed5d36d7b38f87e4aeca777c636bbb3ce9
Sergio Salcedo <trugove@hotmail.com>
2018-03-12 16:12:00 +0100

    MetadataTemplate annotations added.

--- src/main/java/cat/udl/eps/entsoftarch/textannot/domain/MetadataTemplate.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/domain/MetadataTemplate.java
@@ -4,12 +4,19 @@ import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class MetadataTemplate {
 
     @Id @NotBlank
     private String name;
+
+    @OneToMany
+    private List<MetadataField> mtFs;
+
+    @OneToMany
+    private List<Sample> samples;
 
     public String getName() {
         return name;
