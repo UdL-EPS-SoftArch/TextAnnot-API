73682d3e0b1a4fd3ad443670a68ab6cac89e1088
Rogercapcas <rogercapcas@gmail.com>
2018-03-13 17:59:58 +0100

    Only one method in interface MetadataTemplateRepository

--- src/main/java/cat/udl/eps/entsoftarch/textannot/repository/MetadataTemplateRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/repository/MetadataTemplateRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface MetadataTemplateRepository extends PagingAndSortingRepository<MetadataTemplate, String> { }
+public interface MetadataTemplateRepository extends PagingAndSortingRepository<MetadataTemplate, String> {
+    public MetadataTemplate[] findMetadataTemplateByName(String name);
+}
