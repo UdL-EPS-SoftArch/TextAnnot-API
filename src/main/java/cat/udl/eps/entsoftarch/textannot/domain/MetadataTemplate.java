5203af73761a22863eb83bcc9016932985e635b6
Sergio Salcedo <trugove@hotmail.com>
2018-03-14 12:40:27 +0100

    Getters/Setters for time and annotations, done.

--- src/main/java/cat/udl/eps/entsoftarch/textannot/domain/MetadataTemplate.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/domain/MetadataTemplate.java
@@ -1,16 +1,21 @@
 package cat.udl.eps.entsoftarch.textannot.domain;
 
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import java.time.ZonedDateTime;
 
 @Entity
 public class MetadataTemplate {
 
     @Id @NotBlank
     private String name;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private ZonedDateTime startDate;
 
     @OneToMany
     private List<MetadataField> mtFs;
@@ -24,6 +29,30 @@ public class MetadataTemplate {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public ZonedDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(ZonedDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public List<MetadataField> getMtFs() {
+        return mtFs;
+    }
+
+    public void setMtFs(List<MetadataField> mtFs) {
+        this.mtFs = mtFs;
+    }
+
+    public List<Sample> getSamples() {
+        return samples;
+    }
+
+    public void setSamples(List<Sample> samples) {
+        this.samples = samples;
     }
 
 }
