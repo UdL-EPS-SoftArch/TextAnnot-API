e2fa2bd02cc0ad83ede921327263b8a087cf4e88
Rogercapcas <rogercapcas@gmail.com>
2018-04-05 16:20:16 +0200

    Creating tests for the search MetadataTemplate which defines a MetadataField with a name and type

--- src/main/java/cat/udl/eps/entsoftarch/textannot/repository/MetadataTemplateRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/repository/MetadataTemplateRepository.java
@@ -2,9 +2,13 @@ package cat.udl.eps.entsoftarch.textannot.repository;
 
 import cat.udl.eps.entsoftarch.textannot.domain.MetadataTemplate;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface MetadataTemplateRepository extends PagingAndSortingRepository<MetadataTemplate, String> {
-    public MetadataTemplate[] findMetadataTemplateByName(String name);
+    public List<MetadataTemplate> findMetadataTemplateByName(String name);
+    public List<MetadataTemplate> findByDefinesNameAndDefinesType(@Param("name") String name, @Param("type") String type);
 }
