b2a05a139b2f856513fb7eb5e0f858d1864159a7
Roberto García <roberto@rhizomik.net>
2018-09-19 14:25:38 +0200

    Initialize store with default metadata template used by XML samples

--- src/main/java/cat/udl/eps/entsoftarch/textannot/config/RepositoryRestConfig.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/config/RepositoryRestConfig.java
@@ -1,14 +1,25 @@
 package cat.udl.eps.entsoftarch.textannot.config;
 
-import cat.udl.eps.entsoftarch.textannot.domain.*;
+import cat.udl.eps.entsoftarch.textannot.domain.Admin;
+import cat.udl.eps.entsoftarch.textannot.domain.Linguist;
+import cat.udl.eps.entsoftarch.textannot.domain.MetadataField;
+import cat.udl.eps.entsoftarch.textannot.domain.MetadataTemplate;
+import cat.udl.eps.entsoftarch.textannot.domain.MetadataValue;
+import cat.udl.eps.entsoftarch.textannot.domain.Sample;
+import cat.udl.eps.entsoftarch.textannot.domain.XmlSample;
+import cat.udl.eps.entsoftarch.textannot.repository.MetadataFieldRepository;
+import cat.udl.eps.entsoftarch.textannot.repository.MetadataTemplateRepository;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
-import javax.annotation.PostConstruct;
-
 @Configuration
 public class RepositoryRestConfig extends RepositoryRestConfigurerAdapter {
+
+    @Autowired MetadataTemplateRepository metadataTemplateRepository;
+    @Autowired MetadataFieldRepository metadataFieldRepository;
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
@@ -23,6 +34,38 @@ public class RepositoryRestConfig extends RepositoryRestConfigurerAdapter {
 
     @PostConstruct
     public void init() {
-    }
+        MetadataTemplate template = new MetadataTemplate();
+        template.setName("default");
+        metadataTemplateRepository.save(template);
 
+        String[][] defaultTemplateFields = {
+            {"datos_generales", "número_muestra", "String"},
+            {"datos_generales", "código_informante", "String"},
+            {"datos_generales", "transliteración", "String"},
+            {"datos_generales", "revisión_primera", "String"},
+            {"datos_generales", "revisión_segunda", "String"},
+            {"datos_generales", "etiquetado", "String"},
+            {"datos_muestra", "fecha_recogida", "String"},
+            {"datos_muestra", "palabras", "String"},
+            {"datos_muestra", "género_discursivo", "String"},
+            {"datos_muestra", "observaciones", "String"},
+            {"datos_informante", "nombre", "String"},
+            {"datos_informante", "sexo", "String"},
+            {"datos_informante", "nivel_referencia", "String"},
+            {"datos_informante", "curso", "String"},
+            {"datos_informante", "universidad", "String"},
+            {"datos_informante", "nivel_CET", "String"},
+            {"datos_informante", "estancia_España", "String"},
+            {"datos_informante", "observaciones", "String"},
+        };
+
+        for (String[] fieldData: defaultTemplateFields) {
+            MetadataField field = new MetadataField();
+            field.setCategory(fieldData[0]);
+            field.setName(fieldData[1]);
+            field.setType(fieldData[2]);
+            field.setDefinedIn(template);
+            metadataFieldRepository.save(field);
+        }
+    }
 }
