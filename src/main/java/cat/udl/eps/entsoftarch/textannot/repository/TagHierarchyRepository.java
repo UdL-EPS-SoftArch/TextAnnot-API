e49e900c57e5eda9191cd266f3e1e81ed8ba9de8
MrceL97 <9719marcel@gmail.com>
2018-09-20 16:25:55 +0200

    Added TagHierarchy domain and repository.

---
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/repository/TagHierarchyRepository.java
@@ -0,0 +1,13 @@
+package cat.udl.eps.entsoftarch.textannot.repository;
+
+import cat.udl.eps.entsoftarch.textannot.domain.TagHierarchy;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface TagHierarchyRepository extends PagingAndSortingRepository<TagHierarchy, String> {
+    TagHierarchy findByName(@Param("name") String name);
+}
