48ff786a763d76ebb996c107ed1568f18ad08680
Korakk <oscarlopezluna@gmail.com>
2018-10-03 16:23:31 +0200

    Added new method to the tag repository. (TextAnnot-API#39)
    I've added a findBy method that search all tags in a tag hierarchy.

--- src/main/java/cat/udl/eps/entsoftarch/textannot/repository/TagRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/textannot/repository/TagRepository.java
@@ -1,6 +1,7 @@
 package cat.udl.eps.entsoftarch.textannot.repository;
 
 import cat.udl.eps.entsoftarch.textannot.domain.Tag;
+import cat.udl.eps.entsoftarch.textannot.domain.TagHierarchy;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -10,4 +11,5 @@ import java.util.List;
 @RepositoryRestResource
 public interface TagRepository extends PagingAndSortingRepository<Tag, Integer> {
     List<Tag> findByNameContaining(@Param("name") String name);
+    List<Tag> findByDefinedIn(@Param("tagHierarchy")TagHierarchy tagHierarchy);
 }
