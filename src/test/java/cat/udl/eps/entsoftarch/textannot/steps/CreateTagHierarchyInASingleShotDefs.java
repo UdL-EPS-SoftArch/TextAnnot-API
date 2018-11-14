937c5a0f1751687ce3dbc71148ef0b55745d935d
Guillem96 <guillem.orellana@gmail.com>
2018-11-14 14:29:08 +0100

    Modified tests in order to work with new api endpoint

--- src/test/java/cat/udl/eps/entsoftarch/textannot/steps/CreateTagHierarchyInASingleShotDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/textannot/steps/CreateTagHierarchyInASingleShotDefs.java
@@ -1,16 +1,12 @@
 package cat.udl.eps.entsoftarch.textannot.steps;
 
-import cat.udl.eps.entsoftarch.textannot.domain.Tag;
-import cat.udl.eps.entsoftarch.textannot.repository.TagRepository;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.json.JSONObject;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -21,11 +17,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class CreateTagHierarchyInASingleShotDefs {
 
     private StepDefs stepDefs;
-    private TagRepository tagRepository;
 
-    public CreateTagHierarchyInASingleShotDefs(StepDefs stepDefs, TagRepository tagRepository) {
+    public CreateTagHierarchyInASingleShotDefs(StepDefs stepDefs) {
         this.stepDefs = stepDefs;
-        this.tagRepository = tagRepository;
     }
 
     @When("^I send the \"([^\"]*)\" Tag Hierarchy structure$")
@@ -47,13 +41,13 @@ public class CreateTagHierarchyInASingleShotDefs {
 
     @Then("^Its tags have the correct parent/child relationship$")
     public void itsTagsHaveTheCorrectParentChildRelationship() throws Throwable {
-        List<Tag> son = tagRepository.findByNameContaining("another_son_of_root_1");
-        System.out.println("CONTENT!" + son.get(0).getId());
         stepDefs.mockMvc.perform(
-                get("/tags/"+ son.get(0).getId() + "/parent")
+                get("/tagHierarchies/" + 1 + "/tags")
                         .accept(MediaType.APPLICATION_JSON)
                         .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print())
-                .andExpect(jsonPath("$.name", is(son.get(0).getParent().getName())));
+                .andExpect(jsonPath("$.roots[0].name", is("root1")))
+                .andExpect(jsonPath("$.roots[0].children[0].name", is("son_of_root_1")))
+                .andExpect(jsonPath("$.roots[0].children[0].children[0].name", is("grandchild_of_root1")));
     }
 }
